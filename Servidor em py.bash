4826f75d6c5f08393d9c612ee9e9f5de9c05df1e
Pedro <76980297+PedroHenriqueDutra@users.noreply.github.com>
2024-10-22 16:10:38 -0300

    Create Servidor em py.bash

---
+++ Servidor em py.bash
@@ -0,0 +1,5 @@
+# Se a versão do Python retornada acima for 3.X
+python3 -m http.server
+# No windows, tente "python" em vez de "python3"
+# Se a versão do Python retornada acima for 2.X
+python -m SimpleHTTPServer
